9e91f96f1c278560ededbb89ad967c3f96623181
888Shubham <shubham.kb888@gmail.com>
2024-03-02 15:45:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Write the message here. */
+      <p style={{ textAlign: "center", position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>Welcome to React Course on CodingNinjas.com</p>}
+    </div>
+  );
+}
+
+export default App;
